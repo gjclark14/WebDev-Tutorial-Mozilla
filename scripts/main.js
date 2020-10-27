d4553536eaaf65483e105869d5d7cb8b7fa0fc81
gjclark14 <gjclark14@gmail.com>
2020-10-27 11:01:07 -0700

    i'm so hungry

--- scripts/main.js
+++ scripts/main.js
@@ -2,10 +2,10 @@
 let myImage = document.querySelector('img');
 myImage.onclick = function () {
     let mySrc = myImage.getAttribute('src');
-    if (mySrc === 'images/firefox-icon.png') {
-        myImage.setAttribute('src', 'images/return-of-the-silence.jpg');
+    if (mySrc === 'images/britney_betch.jpeg') {
+        myImage.setAttribute('src', 'images/biden_harris.png');
     } else {
-        myImage.setAttribute('src', 'images/firefox-icon.png');
+        myImage.setAttribute('src', 'images/britney_betch.jpeg');
     }
 }
 
